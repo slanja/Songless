6a7babd956739849f4337a5b7bc04974cc81bd36
slanja <97552526+slanja@users.noreply.github.com>
2024-03-04 21:17:38 +0100

    added custom styling

---
+++ src/main/java/org/example/Styles.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Styles {
+    public void ButtonStyle(JButton button) {
+        button.setFont(new Font("Segoe UI", Font.BOLD, 18));
+        button.setForeground(Color.WHITE);
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+    }
+
+    public void ButtonNoStyle(JButton button) {
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.setBorder(null);
+    }
+
+    public void TextStyle(JLabel label) {
+        label.setFont(new Font("Segoe UI", Font.BOLD, 18));
+    }
+}
